490f1cedb80d5379ed82ca2d8f21f49d1372a3d1
YeOldMan23 <kieren.chua23@gmail.com>
2024-10-08 19:21:17 +0800

    Group Project 1 Init Commit

---
+++ utils/globals.py
@@ -0,0 +1,14 @@
+CLASS_ENCODING = {
+    0: "background",
+    1: "spleen",
+    2: "right_kidney",
+    3: "left_kidney",
+    4: "gallbladder",
+    5: "pancreas",
+    6: "liver",
+    7: "stomach",
+    8: "aorta"
+}
+
+LEARNING_RATE = 0.001
+NUM_EPOCHS    = 100
